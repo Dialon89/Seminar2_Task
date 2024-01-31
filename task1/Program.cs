ddd456a62dc9930d3a23ead1f4b539e84ff7db6b
Dialon <aleksey8996@mail.ru>
2024-01-31 12:11:18 +0300

    added solutions :task1

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: 
+// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+n = Math.Abs(n);
+if (n % 7 == 0 && n % 23 == 0)
+    Console.WriteLine(true);
+else {
+    Console.WriteLine(false);
+}    
